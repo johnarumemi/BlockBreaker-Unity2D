e59483047b4636e8c450aabbbddac1ebacaae99f
John-AI <john.arumemi@gmail.com>
2020-11-15 03:53:29 +0000

    Added score to Scenes

--- Assets/Scripts/GameStatus.cs
+++ Assets/Scripts/GameStatus.cs
@@ -2,17 +2,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameStatus : MonoBehaviour
 {
     // Configuration Parameters
     [Range(0.1f, 2f)][SerializeField] private float gameSpeed = 0.75f;
     [SerializeField] private int pointsPerBlockDestroyed = 83;
-    
+
+    [SerializeField] private TextMeshProUGUI scoreText;
     // state variables
     [SerializeField] private int currentScore = 0;
     
     // Update is called once per frame
+    private void Start()
+    {
+        scoreText.text = currentScore.ToString();
+    }
+
     void Update()
     {
         Time.timeScale = gameSpeed;
@@ -21,5 +28,7 @@ public class GameStatus : MonoBehaviour
     public void AddToScore()
     {
         currentScore += pointsPerBlockDestroyed;
+
+        scoreText.text = currentScore.ToString();
     }
 }
