dd2da9068cc0a9de91a4078196b5a07aa0ddd35d
John-AI <john.arumemi@gmail.com>
2020-11-15 04:48:36 +0000

    implemented singleton pattern on GameStatus class

--- Assets/Scripts/GameStatus.cs
+++ Assets/Scripts/GameStatus.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class GameStatus : MonoBehaviour
 {
@@ -13,7 +14,26 @@ public class GameStatus : MonoBehaviour
     [SerializeField] private TextMeshProUGUI scoreText;
     // state variables
     [SerializeField] private int currentScore = 0;
-    
+
+    private void Awake()
+    { 
+        // Implements Singleton Pattern to GameStatus
+        
+        // notice the plural of FindObject[s]OfType.
+        int gameStatusCount = FindObjectsOfType<GameStatus>().Length;
+        
+        if (gameStatusCount > 1)
+        {    // i.e if this Game Status instance is the Second One, destroy myself
+            gameObject.SetActive(false);
+            Destroy(gameObject);
+        }
+        else
+        {
+            // Do not destroy yourself when the next level loads in the future
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
     // Update is called once per frame
     private void Start()
     {
