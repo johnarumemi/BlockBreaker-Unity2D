688f2165b93fce9edc133f6e23d1dd426df1f2d7
John-AI <john.arumemi@gmail.com>
2020-11-28 02:19:38 +0000

    add docs to Level.cs

--- Assets/Scripts/Level.cs
+++ Assets/Scripts/Level.cs
@@ -3,31 +3,56 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
+/*
+ * Logic
+ *
+ * When Win Condition is met, uses SceneLoader to move to next Scene
+ */
+
+
 public class Level : MonoBehaviour
 {
-
-    [SerializeField] private int breakableBlocks; // Serialized for debugging purposes only (we can see it)
-    [SerializeField] private int winCondition;
+    // ------ Configuration Params ------
     
+    // Serialized for debugging purposes only (we can see it)
+    [SerializeField] private int breakableBlocks; 
+    
+    // Serialized to allow altering the Win Condition
+    [SerializeField] private int winCondition; 
+    
+    // ------ Cached Component References ------
     private SceneLoader sceneLoader;
-
+    
+    // ================================================================
     private void Start()
     {
         winCondition = 0;
-        sceneLoader = FindObjectOfType<SceneLoader>();
+        
+        // Calls a generic function if a particular Type of SceneLoader
+        sceneLoader = FindObjectOfType<SceneLoader>(); 
     }
     
     public void AddBreakableBlock()
     {
+        /*
+         * this method is called by Blocks externally
+         */
         breakableBlocks++;
     }
     
     public void DestroyBreakableBlock()
     {
+        /*
+         * This method is called by Blocks externally
+         */
+        
+        // decrement number of breakable blocks within the level.
         breakableBlocks--;
         
         if(breakableBlocks <= winCondition)
         {
+            // Win condition met, load next scene
             Debug.Log("CONGRATS! You Won!");
             sceneLoader.LoadNextScene();
         }
